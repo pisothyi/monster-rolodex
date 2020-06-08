215f1ca3285cc1bf95af47006dc394241cd88455
unknown <pisothyi@gmail.com>
2020-06-08 17:15:51 +0700

    Updates

---
+++ precache-manifest.73128fc6f0cded58b965131cf17bd9b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded091c1b68ece4d39f12f963ede45c5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c27a3b440adcb80bdc8a",
+    "url": "/monster-rolodex/static/css/main.aaba1e53.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/monster-rolodex/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27a3b440adcb80bdc8a",
+    "url": "/monster-rolodex/static/js/main.eafa4a59.chunk.js"
+  },
+  {
+    "revision": "753b30f80137f55e537c",
+    "url": "/monster-rolodex/static/js/runtime-main.e7a3ea5b.js"
+  }
+]);
